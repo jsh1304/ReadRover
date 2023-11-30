66009c7be70d55b47017a678a88212f7e45a5ea6
jsh1304 <jung3914a@gmail.com>
2023-11-30 12:36:36 +0900

    비밀번호 입력 필드 및 가시성 토글 아이콘 구현

--- app/src/main/java/com/jj/readrover/screens/login/ReaderlLoginScreen.kt
+++ app/src/main/java/com/jj/readrover/screens/login/ReaderlLoginScreen.kt
@@ -1,15 +1,15 @@
 package com.jj.readrover.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -19,23 +19,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.jj.readrover.components.EmailInput
+import com.jj.readrover.components.PasswordInput
 import com.jj.readrover.components.ReaderLogo
 
 // 로그인 화면 구현
+@ExperimentalComposeUiApi
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top) {
             ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false) {email, password ->
+                Log.d("Form", "ReaderLoginScreen: $email, $password")
+            }
         }
     }
 }
@@ -44,7 +53,11 @@ fun ReaderLoginScreen(navController: NavHostController) {
 @ExperimentalComposeUiApi
 @Preview
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
     val email = rememberSaveable { mutableStateOf("") } // 이메일 입력 위한 상태 저장 관리하는 변수
     val password = rememberSaveable { mutableStateOf("") } // 비밀번호 입력 위한 상태 저장 관리하는 변수
     val passwordVisibility = rememberSaveable { mutableStateOf(false) } // 비밀번호 가시성 위한 상태 저장 관리하는 변수
@@ -60,53 +73,23 @@ fun UserForm() {
         .verticalScroll(rememberScrollState()) // 수직 스크롤이 가능하도록 함
     
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        EmailInput(emailState = email, enabled = true, onAction = KeyboardActions {
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
             passwordFocusRequest.requestFocus()
         }) // 이메일 입력 필드를 추가하고, 다음 액션으로 비밀번호 필드에 포커스를 요청
+
+        PasswordInput( // PasswordInput 컴포넌트를 생성
+            modifier = Modifier.focusRequester(passwordFocusRequest), // 이는 사용자가 이 입력 필드를 선택할 때 포커스를 받도록 함
+            passwordState = password, // passwordState는 사용자가 입력한 비밀번호를 저장하는 상태
+            labelId = "비밀번호", // 입력 필드의 레이블을 설정
+            enabled = !loading, // loading 상태에 따라 입력 필드를 활성화하거나 비활성화. loading이 true이면 입력 필드는 비활성화되고, false이면 활성화
+            passwordVisibility = passwordVisibility,  // passwordVisibility는 비밀번호의 가시성을 제어
+            onAction = KeyboardActions { // onAction은 키보드 액션이 발생했을 때 실행되는 콜백 함수
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim()) // valid가 true일 때만 onDone 함수를 호출하도록 설정
+            }
+
+        )
     }
 }
 
-@Composable
-fun EmailInput(
-    modifier: Modifier = Modifier, // UI 요소에 추가할 수 있는 효과를 추가하는 Modifier 객체
-    emailState: MutableState<String>, // 이메일 입력을 위한 상태 저장 변수
-    labelId: String = "이메일", // 입력 필드의 레이블 텍스트
-    enabled: Boolean = true, // 입력 필드가 활성화 여부를 나타내는 값
-    imeAction: ImeAction = ImeAction.Next, // 입력 메서드 액션을 나타내는 객체
-    onAction: KeyboardActions = KeyboardActions.Default // 키보드 액션을 처리하는 객체
-) {
-    InputField(modifier = modifier,
-                valuelState = emailState,
-                labelId = labelId,
-                enabled = enabled,
-                keyboardType = KeyboardType.Email,
-                imeAction = imeAction,
-                onAction = onAction) // 이메일 입력 필드를 구성하는 함수
-}
 
-@Composable
-fun InputField(
-    modifier: Modifier = Modifier,
-    valuelState: MutableState<String>,
-    labelId: String,
-    enabled: Boolean,
-    isSingleLine: Boolean = true, // 입력 필드가 한 줄로 표시되는지 여부를 결정하는 값
-    keyboardType: KeyboardType = KeyboardType.Text,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-) {
-    // 입력 필드를 구성하는 함수
-    OutlinedTextField(value = valuelState.value,
-        onValueChange = { valuelState.value = it},
-        label = { Text(text = labelId)},
-        singleLine = isSingleLine,
-        textStyle = TextStyle(fontSize = 18.sp,
-                            color = MaterialTheme.colors.onBackground),
-        modifier = modifier
-            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
-            .fillMaxWidth(),
-        enabled = enabled,
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction))
-
-
-}
