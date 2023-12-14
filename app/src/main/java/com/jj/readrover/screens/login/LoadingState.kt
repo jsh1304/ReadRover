3dcbd73db16f869309c875cc0fb99e19b43861c5
jsh1304 <jung3914a@gmail.com>
2023-12-15 00:28:26 +0900

    Feat: 로딩 상태 관리를 위한 data class 추가
    다양한 로딩 상태 나타내는 enum class 도입,
    동반 객체에서는 각 상태에 대한 기본값 정의

--- app/src/main/java/com/jj/readrover/screens/login/LoadingState.kt
+++ app/src/main/java/com/jj/readrover/screens/login/LoadingState.kt
@@ -1,6 +1,22 @@
 package com.jj.readrover.screens.login
 
-enum class LoadingState() {
+// 로딩 상태를 나타내는 데이터 클래스
+data class LoadingState(val statue: Status, val message: String? = null) {
 
+    // 동반 객체에서는 상태에 대한 기본값을 정의
+    companion object {
+        val IDLE = LoadingState(Status.IDLE) // 아무 작업도 수행하지 않은 상태
+        val SUCCESS = LoadingState(Status.SUCCESS) // 작업이 성공적으로 완료된 상태
+        val LOADING = LoadingState(Status.LOADING) // 작업이 진행 중인 상태
+        val FAILED = LoadingState(Status.FAILED) // 작업이 실패한 상태
+    }
+
+    // Status 열거형은 가능한 모든 상태를 나열
+    enum class Status {
+        SUCCESS, // 작업이 성공적으로 완료
+        FAILED, // 작업이 실패
+        LOADING, // 작업이 진행 중
+        IDLE // 아무 작업도 수행되지 않음
+    }
 
 }
