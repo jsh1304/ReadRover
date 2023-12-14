1644171e3235edb787af256e9c7b989309eadd69
jsh1304 <jung3914a@gmail.com>
2023-12-15 00:21:48 +0900

    Docs: 로그인 화면의 ViewModel 주석 추가
    이메일과 비밀번호로 로그인하는 기능에 대한 설명 추가

--- app/src/main/java/com/jj/readrover/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/jj/readrover/screens/login/LoginScreenViewModel.kt
@@ -8,29 +8,36 @@ import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
+// 로그인 화면의 뷰 모델을 나타냄
 class LoginScreenViewModel: ViewModel() {
     //val loadingDtate = MutableStateFlow(LoadingState.IDLE)
-    private val auth: FirebaseAuth = Firebase.auth
+    private val auth: FirebaseAuth = Firebase.auth // Firebase 인증 객체
 
+    /**
+     * LiveDate는 값을 관찰하고 사용 O, 값 변경 X
+     * MutableLiveDate는 값 변경 O
+     */
+    // 로딩 상태를 나타냄
     private val _loading = MutableLiveData(false)
     val loading: LiveData<Boolean> = _loading
 
+    // 이메일과 비밀번호로 로그인을 시도
     fun signInWitEmailAndPassword(email: String, password: String)
     = viewModelScope.launch{
         try {
+            // Firebase 인증을 사용하여 이메일과 비밀번호로 로그인을 시도
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
-                    if (task.isSuccessful){
+                    if (task.isSuccessful){ // 작업이 성공적으로 완료하면
 
-                    } else {
+                    } else { // 작업이 실패하면
                         Log.d("FB", "signInWitEmailAndPassword: ${task.result.toString()}")
                     }
                 }
-        } catch (ex: Exception) {
+        } catch (ex: Exception) { // 예외가 발생하면 예외 메시지를 로그로 출력
             Log.d("FB", "signInWitEmailAndPassword: ${ex.message}")
         }
     }
