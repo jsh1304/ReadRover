c856257381e58527a72bc18b0e23a15ef76e653f
jsh1304 <jung3914a@gmail.com>
2023-12-15 12:21:28 +0900

    Feat: 로그인 성공 시 홈화면 이동 구현
    로그인 화면에서 signInWitEmailAndPassword메서드를 호출하여 이메일과 비밀번호로 로그인 시도. 로그인이 성공하면 navController를 사용하여 홈 화면(ReaderHomeScreen)으로 이동.

--- app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,10 @@
 package com.jj.readrover.screens.home
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController) {
-
+    Text(text = "홈화면")
 }
