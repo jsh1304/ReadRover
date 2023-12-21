ede6b4617f42a8fb31ba6898ad817aa19f5af9a6
jsh1304 <jung3914a@gmail.com>
2023-12-21 23:59:35 +0900

    Home 및 FABContent 추가: 홈 화면 및 플로팅 액션 버튼 구현

--- app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,45 @@
 package com.jj.readrover.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController) {
-    Text(text = "홈화면")
+    Scaffold(topBar = {},
+            floatingActionButton = {
+                FABContent{
+                    
+                }
+            }) {
+            
+            Surface(modifier = Modifier.fillMaxSize()) {
+                
+            }
+    }
+}
+
+
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    FloatingActionButton(onClick = { onTap("") },
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = MaterialTheme.colors.background) {
+        Icon(imageVector = Icons.Default.Add,
+            contentDescription = "책 추가",
+            tint = MaterialTheme.colors.onSecondary)
+    }
+
 }
