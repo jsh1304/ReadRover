ca23a7c494c476f0cd6764027bb26259263064d6
jsh1304 <jung3914a@gmail.com>
2023-12-27 12:18:54 +0900

    Feat: 홈 화면의 책 리스트 카드 기능 구현
    - Coil 라이브러리 추가
    - 홈 화면의 책 카드 생성. 책 제목, 이미지 표시

--- app/src/main/java/com/jj/readrover/components/Components.kt
+++ app/src/main/java/com/jj/readrover/components/Components.kt
@@ -1,26 +1,38 @@
 package com.jj.readrover.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.jj.readrover.navigation.ReaderScreens
 
 // 앱 로고 구현
 @Composable
@@ -122,5 +134,80 @@ fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
     // IconButton 컴포넌트를 사용하여 아이콘 버튼을 생성
     IconButton(onClick = { passwordVisibility.value = !visible }) { // 사용자가 아이콘 버튼을 클릭하면 비밀번호의 가시성 상태를 토글
         Icons.Default.Close
+    }
+}
+
+//앱 바를 구성
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController
+) {
+
+    // TopAppBar: 앱 바의 레이아웃과 스타일을 설정
+    TopAppBar(
+        title = {
+            // Row: 가로 방향으로 요소를 배치
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                // showProfile이 true인 경우, 프로필 아이콘을 표시
+                if (showProfile) {
+                    Icon(imageVector = Icons.Default.Favorite,
+                        contentDescription = "Logo Icon",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .scale(0.9f))
+                }
+                // 앱 바의 제목 텍스트를 설정
+                Text(text = title,
+                    color = Color.Red.copy(alpha = 0.7f),
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp))
+            }
+        },
+        // actions: 앱 바의 액션을 설정
+        actions = {
+            IconButton(onClick = { // 아이콘 클릭 시 사용자 로그아웃
+                FirebaseAuth.getInstance().signOut().run {
+                    // 로그아웃 후에 로그인 화면으로 이동
+                    navController.navigate(ReaderScreens.LoginScreen.name)
+                }
+            }) {
+                // 로그아웃 아이콘
+                Icon(imageVector = Icons.Filled.Logout,
+                    contentDescription = "Logout",
+                    tint = Color.Green.copy(alpha = 0.4f))
+            }
+        },
+        // backgroundColor: 앱 바의 배경색을 설정
+        backgroundColor = Color.Transparent,
+        // elevation: 앱 바의 그림자 높이를 설정
+        elevation = 0.dp)
+}
+
+// 타이틀을 보여주는 UI 컴포넌트
+@Composable
+fun TitleSection(modifier: Modifier = Modifier,
+                 label: String) {
+    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
+        Column {
+            Text(text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Left)
+        }
+    }
+}
+
+// 플로팅 액션 버튼의 내용을 구성
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    // FloatingActionButton: 플로팅 액션 버튼의 레이아웃과 스타일을 설정
+    FloatingActionButton(onClick = { onTap("") },
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = MaterialTheme.colors.background) {
+        // Icon: 플로팅 액션 버튼의 아이콘을 설정
+        Icon(imageVector = Icons.Default.Add,
+            contentDescription = "책 추가",
+            tint = MaterialTheme.colors.onSecondary)
     }
 }
--- app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/jj/readrover/screens/home/ReaderHomeScreen.kt
@@ -1,33 +1,28 @@
 package com.jj.readrover.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
+import com.jj.readrover.components.FABContent
+import com.jj.readrover.components.ReaderAppBar
+import com.jj.readrover.components.TitleSection
 import com.jj.readrover.model.MBook
 import com.jj.readrover.navigation.ReaderScreens
 
@@ -91,82 +86,61 @@ fun HomeContent(navController: NavController) {
     }
 }
 
-//앱 바를 구성
-@Composable
-fun ReaderAppBar(
-    title: String,
-    showProfile: Boolean = true,
-    navController: NavController
-) {
-
-    // TopAppBar: 앱 바의 레이아웃과 스타일을 설정
-    TopAppBar(
-        title = {
-            // Row: 가로 방향으로 요소를 배치
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                // showProfile이 true인 경우, 프로필 아이콘을 표시
-                if (showProfile) {
-                    Icon(imageVector = Icons.Default.Favorite,
-                        contentDescription = "Logo Icon",
-                        modifier = Modifier
-                            .clip(RoundedCornerShape(12.dp))
-                            .scale(0.9f))
-                }
-                // 앱 바의 제목 텍스트를 설정
-                Text(text = title,
-                    color = Color.Red.copy(alpha = 0.7f),
-                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp))
-            }
-        },
-        // actions: 앱 바의 액션을 설정
-        actions = {
-                  IconButton(onClick = { // 아이콘 클릭 시 사용자 로그아웃
-                      FirebaseAuth.getInstance().signOut().run {
-                          // 로그아웃 후에 로그인 화면으로 이동
-                          navController.navigate(ReaderScreens.LoginScreen.name)
-                      }
-                  }) {
-                      // 로그아웃 아이콘
-                    Icon(imageVector = Icons.Filled.Logout,
-                        contentDescription = "Logout",
-                        tint = Color.Green.copy(alpha = 0.4f))
-                  }
-        },
-        // backgroundColor: 앱 바의 배경색을 설정
-        backgroundColor = Color.Transparent,
-        // elevation: 앱 바의 그림자 높이를 설정
-        elevation = 0.dp)
-}
-
-// 타이틀을 보여주는 UI 컴포넌트
-@Composable
-fun TitleSection(modifier: Modifier = Modifier,
-                label: String) {
-    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
-        Column {
-            Text(text = label,
-                fontSize = 19.sp,
-                fontStyle = FontStyle.Normal,
-                textAlign = TextAlign.Left)
-        }
-    }
-}
 
 @Composable
 fun ReadingRightNowArea(books: List<MBook>, navController: NavHostController) {
 
 }
 
-// 플로팅 액션 버튼의 내용을 구성
+
+// 리스트에 있는 책의 카드를 생성
+@Preview
 @Composable
-fun FABContent(onTap: (String) -> Unit) {
-    // FloatingActionButton: 플로팅 액션 버튼의 레이아웃과 스타일을 설정
-    FloatingActionButton(onClick = { onTap("") },
-        shape = RoundedCornerShape(50.dp),
-        backgroundColor = MaterialTheme.colors.background) {
-        // Icon: 플로팅 액션 버튼의 아이콘을 설정
-        Icon(imageVector = Icons.Default.Add,
-            contentDescription = "책 추가",
-            tint = MaterialTheme.colors.onSecondary)
+fun ListCard(book: MBook = MBook("asd", "kotlin in action", "사람", "hi"),
+            onPressDetails: (String) -> Unit = {}) { // onPressDetails는 카드가 클릭되었을 때 호출되는 함수
+    val context = LocalContext.current
+
+    // resources: 디바이스의 환경설정, 화면 크기, 밀도 등에 따라 동적으로 변화할 수 있는 값들을 제공
+    val resources = context.resources
+
+    // displayMetrics: 디바이스 화면의 여러 가지 메트릭(치수) 정보를 담고 있음.
+    // 화면의 너비, 높이, 밀도, 폰트 크기 등을 포함
+    val displayMetrics = resources.displayMetrics
+
+    /*
+    * 디바이스 화면의 너비를 밀도 독립적인 픽셀(dp) 단위로 변환하는 공식
+    * 이렇게 하면 다양한 화면 크기와 해상도를 가진 디바이스에서도 일관된 레이아웃과 디자인을 유지할 수 있음
+    * */
+    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
+
+    val spacing = 10.dp
+
+    // 책 카드 생성
+    Card(shape = RoundedCornerShape(30.dp),
+        backgroundColor = Color.LightGray,
+        elevation = 50.dp,
+        modifier = Modifier
+            .padding(15.dp)
+            .height(240.dp)
+            .width(200.dp)
+            .clickable { onPressDetails.invoke(book.title.toString()) }) { // 클릭 시 책 상세 정보를 제공
+        Column(modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(horizontalArrangement = Arrangement.Center) {
+                // 책 이미지 생성
+                Image(painter = rememberImagePainter(data = ""),
+                    contentDescription = "book image",
+                    modifier = Modifier
+                        .height(140.dp)
+                        .width(100.dp)
+                        .padding(4.dp))
+                Spacer(modifier = Modifier.width(50.dp)) // 화면의 가로 길의 50dp 공간 만들기
+            }
+        }
+
     }
+
 }
+
+
+
