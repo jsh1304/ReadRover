01723ed97275fcf8e0cc118b93c4b9b554eb6d04
jsh1304 <jung3914a@gmail.com>
2024-01-17 12:55:34 +0900

    Refactor: BookSearchViewModel 개선
    - DataOrException에서 Resource로 상태 관리 간소화
    - 오류 처리 개선: when 문 사용
    - 사용하지 않는 Import 제거

--- app/src/main/java/com/jj/readrover/screens/search/BookSearchViewModel.kt
+++ app/src/main/java/com/jj/readrover/screens/search/BookSearchViewModel.kt
@@ -1,26 +1,28 @@
 package com.jj.readrover.screens.search
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jj.readrover.data.DataOrException
+import com.jj.readrover.data.Resource
 import com.jj.readrover.model.Item
 import com.jj.readrover.repository.BookRepository
-import com.squareup.okhttp.Dispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BookSearchViewModel @Inject constructor(private val repository: BookRepository):
 ViewModel(){
-    val listOfBooks: MutableState<DataOrException<List<Item>, Boolean, Exception>>
-        = mutableStateOf(DataOrException(null, true, Exception("")))
+    var listOfBooks: List<Item> by mutableStateOf(listOf())
 
     init {
+        loadBooks()
+    }
+
+    private fun loadBooks() {
         searchBooks("android")
     }
 
@@ -29,12 +31,19 @@ ViewModel(){
             if (query.isEmpty()) {
                 return@launch
             }
-            listOfBooks.value.loading = true // 로딩 실행
-            listOfBooks.value = repository.getBooks(query) // 검색 쿼리에 대한 값을 저장
-            Log.d("Search", "searchBooks: ${listOfBooks.value.data.toString()}")
-
-            // 검색 쿼리에 대한 결과 값이 존재하면 로딩 종료
-            if (listOfBooks.value.data.toString().isNotEmpty()) listOfBooks.value.loading = false
+            try {
+                when(val response = repository.getBooks(query)) {
+                    is Resource.Success -> {
+                        listOfBooks = response.data!!
+                    }
+                    is Resource.Error -> {
+                        Log.e("Network", "searchBooks: 책 검색 실패", )
+                    }
+                    else -> {}
+                }
+            } catch (exception: Exception) {
+                Log.d("Network", "searchBooks: ${exception.message.toString()}")
+            }
 
         }
     }
