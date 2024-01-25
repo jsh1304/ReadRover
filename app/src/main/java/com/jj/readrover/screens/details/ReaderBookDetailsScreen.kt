7c61b81f4308c655ca10afa8165e766e2faa8b92
jsh1304 <jung3914a@gmail.com>
2024-01-25 11:36:53 +0900

    Feat: 책 상세정보 추가 표시 - 책 이미지, 저자, 쪽수, 카테고리, 발행일

--- app/src/main/java/com/jj/readrover/screens/details/ReaderBookDetailsScreen.kt
+++ app/src/main/java/com/jj/readrover/screens/details/ReaderBookDetailsScreen.kt
@@ -1,23 +1,21 @@
 package com.jj.readrover.screens.details
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.jj.readrover.components.ReaderAppBar
 import com.jj.readrover.data.Resource
 import com.jj.readrover.model.Item
@@ -52,8 +50,12 @@ fun BookDetailsScreen(
                 }.value
 
                 if (bookInfo.data == null) { // 책 정보 없을 경우
-                    LinearProgressIndicator()
+                    Row() {
+                        LinearProgressIndicator()
+                        Text(text = "로딩 중")
+                    }
                 } else { // 책 정보 존재 할 경우
+                    ShowBookDetails(bookInfo, navController)
                     Text(text = "책 id: ${bookInfo.data.volumeInfo.title}")
                 }
                 Log.d("TAG", "BookDetailsScreen: ${bookInfo.data.toString()}")
@@ -61,4 +63,33 @@ fun BookDetailsScreen(
         }
 
     }
+}
+
+@Composable
+fun ShowBookDetails(bookInfo: Resource<Item>, navController: NavController) {
+    val bookData =  bookInfo.data?.volumeInfo
+    val googleBookId = bookInfo.data?.id
+
+    Card(modifier = Modifier.padding(34.dp),
+        shape = CircleShape,
+        elevation = 4.dp) {
+        Image(painter = rememberImagePainter(data = bookData!!.imageLinks.thumbnail),
+            contentDescription = "Book image",
+            modifier = Modifier
+                .width(90.dp)
+                .height(90.dp)
+                )
+    }
+    
+    Text(text = bookData?.title.toString(),
+        style = MaterialTheme.typography.h6,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 15)
+    
+    Text(text = "저자: ${bookData?.authors.toString()}")
+    Text(text = "쪽수: ${bookData?.pageCount.toString()}")
+    Text(text = "카테고리: ${bookData?.categories.toString()}",
+        style = MaterialTheme.typography.subtitle1)
+    Text(text = "발행일: ${bookData?.publishedDate.toString()}",
+        style = MaterialTheme.typography.subtitle1)
 }
